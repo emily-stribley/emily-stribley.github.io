e6d830f7ff968aa7fbd8f76d16da155b84b05ccd
emily-stribley <enstr2@student.monash.edu>
2017-11-16 13:13:13 +1100

    Finalise Folio

--- folio/assets/js/main.js
+++ folio/assets/js/main.js
@@ -11,6 +11,7 @@ $( ".customercopy" ).hover(
   }
 );
 
+
 $( ".rad" ).hover(
   function() {
     $( ".rad_image" ).addClass( "show" );
@@ -19,6 +20,62 @@ $( ".rad" ).hover(
   }
 );
 
+
+$( ".chadstone" ).hover(
+  function() {
+    $( ".chadstone_image" ).addClass( "show" );
+  }, function() {
+    $( ".chadstone_image" ).removeClass( "show" );
+  }
+);
+
+$( ".subtitled" ).hover(
+  function() {
+    $( ".subtitled_image" ).addClass( "show" );
+  }, function() {
+    $( ".subtitled_image" ).removeClass( "show" );
+  }
+);
+
+$( ".esperanto" ).hover(
+  function() {
+    $( ".esperanto_image" ).addClass( "show" );
+  }, function() {
+    $( ".esperanto_image" ).removeClass( "show" );
+  }
+);
+
+$( ".architecture" ).hover(
+  function() {
+    $( ".architecture_image" ).addClass( "show" );
+  }, function() {
+    $( ".architecture_image" ).removeClass( "show" );
+  }
+);
+
+$( ".cityguide" ).hover(
+  function() {
+    $( ".cityguide_image" ).addClass( "show" );
+  }, function() {
+    $( ".cityguide_image" ).removeClass( "show" );
+  }
+);
+
+$( ".diffuser" ).hover(
+  function() {
+    $( ".diffuser_image" ).addClass( "show" );
+  }, function() {
+    $( ".diffuser_image" ).removeClass( "show" );
+  }
+);
+
+$( ".events" ).hover(
+  function() {
+    $( ".events_image" ).addClass( "show" );
+  }, function() {
+    $( ".events_image" ).removeClass( "show" );
+  }
+);
 
 
 
